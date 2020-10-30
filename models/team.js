4aac70f57bd052f7cdf866c6cd30acde6995d906
jacheampong <jacheampong@gmail.com>
2020-10-30 12:04:02 -0400

    team model update

---
+++ models/team.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+// const colorSchema = new mongoose.Schema({
+//     name: String,
+// });
+
+const teamSchema = new mongoose.Schema({
+    teamName: { type: String, required: true },
+    manager: { type: String, required: true},
+
+    // embed color in team
+    // teamColors: [colorSchema],
+    teamColors: [String],
+    players: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Player',
+    },],
+});
+
+const Team = mongoose.model('Team', teamSchema);
+// const Color = mongoose.model('Color', colorSchema);
+
+module.exports = Team
