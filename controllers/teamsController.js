5e9291c62345e695d6259a6a23daecdb1d9080e9
jacheampong <jacheampong@gmail.com>
2020-10-30 12:31:41 -0400

    teams index route and controller update

--- controllers/teamsController.js
+++ controllers/teamsController.js
@@ -1,5 +1,13 @@
 const router = require('express').Router()
 const Team = require('../models/team')
 
+// teams index page
+router.get('/', async (req, res) => {
+    let teams = await Team.find({})
+    res.render('teams/index.ejs', { 
+        teams 
+    })
+})
+
 
 module.exports = router
