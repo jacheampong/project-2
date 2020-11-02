778366ee77ba70bce050f2ca582ff76f9cb9e49c
jacheampong <jacheampong@gmail.com>
2020-11-02 13:56:46 -0500

    initial user post route

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -3,9 +3,17 @@ const express = require('express')
 const router = express.Router()
 const User = require('../models/user.js')
 
+// new user route
 router.get('/new', (req, res) => {
     res.render('users/new.ejs')
 })
 
+// post route
+router.post('/', (req, res) => {
+    User.create(req.body, (err, createdUser) => {
+        console.log('user is created', createdUser)
+        res.redirect('/')
+    })
+})
 
 module.exports = router
